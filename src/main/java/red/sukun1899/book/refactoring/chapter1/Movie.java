a7f434e9662929139a7dfcd28a117be788509e1d
su-kun1899 <higedrum.coz@gmail.com>
2018-05-25 12:26:14 +0900

    getChargeはMovieクラスに持たせる

--- src/main/java/red/sukun1899/book/refactoring/chapter1/Movie.java
+++ src/main/java/red/sukun1899/book/refactoring/chapter1/Movie.java
@@ -18,6 +18,28 @@ public class Movie {
         this.priceCode = priceCode;
     }
 
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
     public int getPriceCode() {
         return priceCode;
     }
