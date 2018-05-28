ebcb0a1a4aa7b147032ad9cac62e48cd38cee0d9
su-kun1899 <higedrum.coz@gmail.com>
2018-05-28 14:14:40 +0900

    メソッドの移動

--- src/main/java/red/sukun1899/book/refactoring/chapter1/Price.java
+++ src/main/java/red/sukun1899/book/refactoring/chapter1/Price.java
@@ -5,6 +5,28 @@ package red.sukun1899.book.refactoring.chapter1;
  */
 abstract class Price {
     abstract int getPriceCode();
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
 }
 
 class ChildrenPrice extends Price {
