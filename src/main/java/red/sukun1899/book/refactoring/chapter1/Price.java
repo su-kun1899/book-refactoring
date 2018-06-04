ffb4d24fe5bf3812bc28822ab5d8bf84f8770214
su-kun1899 <higedrum.coz@gmail.com>
2018-06-04 17:51:50 +0900

    getChargeでポリモーフィズムによる条件記述の書き換え

--- src/main/java/red/sukun1899/book/refactoring/chapter1/Price.java
+++ src/main/java/red/sukun1899/book/refactoring/chapter1/Price.java
@@ -6,33 +6,22 @@ package red.sukun1899.book.refactoring.chapter1;
 abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrenPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.CHILDREN;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5;
+        }
+        return result;
     }
 }
 
@@ -41,11 +30,25 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result;
     }
 }
---
+++ src/test/groovy/red/sukun1899/book/refactoring/chapter1/PriceSpec.groovy
@@ -0,0 +1,71 @@
+package red.sukun1899.book.refactoring.chapter1;
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author su-kun1899
+ */
+class PriceSpec extends Specification {
+    Price price
+
+    @Unroll
+    def '通常価格で #daysRented 日レンタルの場合、料金は #expected になる'() {
+        given:
+        price = new RegularPrice()
+
+        when:
+        def actual = price.getCharge(daysRented)
+
+        then:
+        actual == expected
+
+        where:
+        daysRented || expected
+        1          || 2
+        2          || 2
+        3          || 2 + 1.5 * 1
+        4          || 2 + 1.5 * 2
+        5          || 2 + 1.5 * 3
+    }
+
+    @Unroll
+    def '子供向け価格で #daysRented 日レンタルの場合、料金は #expected になる'() {
+        given:
+        price = new ChildrenPrice()
+
+        when:
+        def actual = price.getCharge(daysRented)
+
+        then:
+        actual == expected as double
+
+        where:
+        daysRented || expected
+        1          || 1.5
+        2          || 1.5
+        3          || 1.5
+        4          || 1.5 + 1.5 * 1
+        5          || 1.5 + 1.5 * 2
+    }
+
+    @Unroll
+    def '新作価格で #daysRented 日レンタルの場合、料金は #expected になる'() {
+        given:
+        price = new NewReleasePrice()
+
+        when:
+        def actual = price.getCharge(daysRented)
+
+        then:
+        actual == expected
+
+        where:
+        daysRented || expected
+        1          || 3
+        2          || 3 * 2
+        3          || 3 * 3
+        4          || 3 * 4
+        5          || 3 * 5
+    }
+}
